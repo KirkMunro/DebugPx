6cd85378c04d59d1edbeb84d9e4aeccdcd3429a2
Kirk Munro <KirkMunro@users.noreply.github.com>
2014-10-01 11:35:28 -0400

    initial commit

---
+++ DebugPx/InvokeIfDebugCommand.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace DebugPx
+{
+    [Cmdlet(
+        VerbsLifecycle.Invoke,
+        "IfDebug"
+    )]
+    [OutputType(typeof(PSObject))]
+    public class InvokeIfDebugCommand : PSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+			Mandatory = true,
+            HelpMessage = "The debug script that will be invoked if DebugPreference is anything other than Ignore or SilentlyContinue."
+        )]
+        [ValidateNotNull()]
+        [Alias("ScriptBlock", "sb")]
+        public ScriptBlock DebugScript;
+
+        [Parameter(
+            ValueFromPipeline = true,
+            HelpMessage = "The object that was input to the command, either by value (using this parameter) or through the pipeline."
+        )]
+        [ValidateNotNull()]
+        public PSObject InputObject;
+
+        protected override void ProcessRecord()
+        {
+			// Get the DebugPreference variable
+			PSVariable debugPreferenceVariable = SessionState.PSVariable.Get("DebugPreference");
+
+			// If the DebugPreference variable is not of type ActionPreference, send back a warning and return immediately
+			if (!(debugPreferenceVariable.Value is ActionPreference))
+            {
+				WriteWarning("$DebugPreference is not of type System.Management.Automation.ActionPreference. The ifdebug command does not support this unexpected configuration.");
+				return;
+			}
+
+			// If DebugPreference is set to SilentlyContinue or Ignore, return immediately
+			ActionPreference debugPreference = (ActionPreference)debugPreferenceVariable.Value;
+			if (ActionPreference.Ignore == debugPreference || ActionPreference.SilentlyContinue == debugPreference)
+            {
+                return;
+            }
+
+			// If DebugPreference is set to Inquire, temporarily set it to continue, and then invoke our script block
+            ActionPreference originalDebugPreference = debugPreference;
+			try
+            {
+				// Make sure the debug preference is not Inquire
+				if (ActionPreference.Inquire == debugPreference)
+                {
+					debugPreference = ActionPreference.Continue;
+					SessionState.PSVariable.Set("DebugPreference", debugPreference);
+				}
+
+				// Create the object used to invoke PowerShell commands in the current runspace
+				PowerShell ps = PowerShell.Create(RunspaceMode.CurrentRunspace);
+
+				// If we received pipeline input, we must set up the _ and PSItem variables as if using a process block
+				if (MyInvocation.BoundParameters.ContainsKey("InputObject"))
+				{
+					SessionState.PSVariable.Set("_", InputObject);
+					SessionState.PSVariable.Set("PSItem", InputObject);
+				}
+
+				// Invoke the debug Script Block and send all output in string format to Write-Debug
+				ps.AddScript(DebugScript.ToString(), false);
+                ps.AddCommand("Out-String");
+                ps.AddParameter("Stream");
+                ps.AddCommand("Write-Debug");
+				ps.Invoke();
+				if (ps.HadErrors)
+				{
+					foreach (ErrorRecord error in ps.Streams.Error)
+					{
+						WriteError(error);
+					}
+				}
+			}
+			finally
+            {
+				// Reset the DebugPreference value if necessary
+				if (debugPreference != originalDebugPreference)
+                {
+					SessionState.PSVariable.Set("DebugPreference", originalDebugPreference);
+                }
+			}
+
+            // If the caller used the InputObject parameter, pass that down the pipeline if we got this far
+            if (MyInvocation.BoundParameters.ContainsKey("InputObject"))
+            {
+                WriteObject(InputObject);
+            }
+        }
+    }
+}
---
+++ DebugPx/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DebugPx")]
+[assembly: AssemblyDescription("The DebugPx module provides a set of commands that make it easier to debug PowerShell scripts, functions and modules. These commands leverage the native debugging capabilities in PowerShell (the callstack, breakpoints, error output and the -Debug common parameter) and provide additional functionality that these features do not provide, enabling a richer debugging experience.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kirk Munro")]
+[assembly: AssemblyProduct("DebugPx")]
+[assembly: AssemblyCopyright("Copyright © 2014 Kirk Munro")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8eecd24a-92e6-45c5-becc-931ade7abb30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
